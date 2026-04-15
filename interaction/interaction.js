5ce56d4812508ecf1b440b4ce52b706cb817d1eb
avaneee <avaneedalmia19@gmail.com>
2026-04-15 09:05:29 -0400

    4/15 loop and interaction

--- interaction/interaction.js
+++ interaction/interaction.js
@@ -5,14 +5,41 @@ let playButton = document.getElementById("play");
 // connecting to speakers
 let synth = new Tone.Synth().toDestination();
 
+// older code
+// playButton.addEventListener("click", () => {
+//     if (Tone.context.state !== "running"){
+//         Tone.start();
+//     }
+//     synth.triggerAttackRelease("C3","8n"); 
+// });
 
+// mapping keyboard keys to their notes
+let keyMap = {
+    "1": "C3",
+    "2": "D3",
+    "3": "E3",
+    "4": "F3",
+    "5": "G3",
+    "6": "A3",
+    "7": "B3",
+    "8": "C4"
+};
+
+// start audio context on button click 
 playButton.addEventListener("click", () => {
-    if (Tone.context.state !== "running"){
+    if (Tone.context.state !== "running") {
         Tone.start();
     }
-    synth.triggerAttackRelease("C3","8n"); 
 });
 
+// listen for keyboard presses
+document.addEventListener("keydown", (event) => {
+    let key = event.key;
+
+    if (keyMap[key]) {
+        synth.triggerAttackRelease(keyMap[key], "8n");
+    }
+});
 
 // tutorial on yt: https://www.youtube.com/watch?v=7ZhbKclhDf4
 
--- loop/loop.js
+++ loop/loop.js
@@ -1,12 +1,41 @@
-// let words = document.querySelectorAll('p');
-// function randomizeEverything() {
-// 	words.forEach((element) => {
-// 		let red = Math.round(Math.random()*255);
-// 		let green = Math.round(Math.random()*255);
-// 		let blue = Math.round(Math.random()*255);
-// 		element.style.color = `rgb(${red},${green},${blue})`;
-// 		element.style.backgroundColor = `rgb(${255-red},${255-green},${255-blue})`;
-// 		element.style.transform = `rotate(${Math.random()*720-360}deg)`;
-// 	})
-// }
-// setInterval(randomizeEverything, 1000)
+let container = document.querySelector('.container');
+
+let images = [
+    "cd1.png",
+    "cd2.png"
+];
+
+let rotations = [20, 30, 8, 10, 5]
+let speeds = ["12", "50", "10", "2"]
+
+// CDs
+for (let i = 0; i < 60; i++) {
+    let randomSize = Math.random() * 100 + 50;
+    let randomX = Math.random() * window.innerWidth;
+    let randomY = Math.random() * window.innerHeight;
+    let randomSpeed = Math.random() * 10;
+
+    let randomImage = images[Math.floor(Math.random() * images.length)];
+
+    rotations.push(0);
+    speeds.push(randomSpeed);
+
+    let newCD = `<img src="${randomImage}
+    "style="
+		width: ${randomSize}px;
+		left: ${randomX}px;
+		top: ${randomY}px;
+		position: absolute;">`;
+    container.innerHTML += newCD;
+}
+
+function rotateCDs() {
+    let cds = document.querySelectorAll('.container img');
+
+    cds.forEach(function (cd, index) {
+        rotations[index] = rotations[index] + speeds[index];
+        cd.style.transform = "rotate(" + rotations[index] + "deg)";
+    });
+}
+
+setInterval(rotateCDs, 50);

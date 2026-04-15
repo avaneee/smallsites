55d0b310442be8dca2c3091f779822d998507d4a
avaneee <avaneedalmia19@gmail.com>
2026-04-15 11:30:43 -0400

    almost done? 4/15

--- homepage.js
+++ homepage.js
@@ -8,3 +8,13 @@
 // 	})
 // }
 // setInterval(changeLinkSizes, 1000);
+//
+
+function rotateLinks() {
+	let allSpans = document.querySelectorAll('span');
+	allSpans.forEach((element) => {
+		let randomDeg = Math.random()*20;
+		element.style.rotate = randomDeg + "deg";
+	})
+}
+setInterval(rotateLinks, 500);
